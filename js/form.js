797fd6af6ed8de3a347855beb55a9d0b869d8ce4
sai3010 <saiprasanth2007@gmail.com>
2018-02-21 00:13:43 +0530

    done

--- js/form.js
+++ js/form.js
@@ -1,107 +1,105 @@
 // Initialize Firebase
-    var config = {
-    apiKey: "AIzaSyAqIvSVJoknpvLJHG6Qn0om0JDfGVgJqdE",
-    authDomain: "webapp-faf2d.firebaseapp.com",
-    databaseURL: "https://webapp-faf2d.firebaseio.com",
-    projectId: "webapp-faf2d",
-    storageBucket: "webapp-faf2d.appspot.com",
-    messagingSenderId: "294281955476"
-    };
-    firebase.initializeApp(config);
-    var id;
-    var databaseRef = firebase.database().ref('users/');
-    var db='/users/';
-    /*function save()     //to create ticket number db
-    {
-    var databaseRef = firebase.database().ref('ticketnumber/');
-    var db='/ticketnumber/';
-    var tid=0;
-    var data = {
-                tid:tid
-            }
-
-            var updates = {};
-            updates[db + tid] = data;            // + usn makes it a primary key.
-            firebase.database().ref().update(updates);
-            alert('success');
-
-            reload_page();
-    }*/
-    function tstore()
-    {
-    var databaseRef = firebase.database().ref('ticketnumber/');
-    var db='/ticketnumber/';
-    var data = {
-          tid:id
-           }
-    var updates = {};
-            updates[db + 0] = data;         
-            firebase.database().ref().update(updates);
-   }
-     function save_user() {
-      var databaseRef = firebase.database().ref('ticketnumber/');
-    var db='/ticketnumber/';
-    databaseRef.once('value', function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
-                var childData = childSnapshot.val();  //gets all the child data
-                id=childData.tid;
-                /*alert("first id");
-                alert(id);*/
-            });
-             }).then(function(){
-            var databaseRef = firebase.database().ref('users/');
-            var db='/users/';
-            var gen = document.getElementById("gender");
-            var mod = document.getElementById("mod");
-            var strgen = gen.options[gen.selectedIndex].text.toUpperCase();
-            var strmod = mod.options[mod.selectedIndex].text.toUpperCase();
-
-            var name = document.getElementById('fname').value.toUpperCase();
-            var email = document.getElementById('email').value.toUpperCase();
-            var phone = document.getElementById('mob').value;
-            id=id+1;
-            var tid="T"+id;
-            var data = {
-                tid:tid,
-                name:name,
-                email:email,
-                phone:phone,
-                gender:strgen,
-                mop:strmod
-              }
-
-            var updates = {};
-            updates[db + tid] = data;            
-            firebase.database().ref().update(updates);
-            alert('successfully Registered');
-            tstore();
-            change_page();
-        });
-  }
-function change_page() {
-    window.location.href="ticket.html";
-   }
-
-function logout(){
-  firebase.auth().signOut();
-  window.location="index.html";
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyCdptDABvFMyrRx_Sr4Zx7zlbeCtoawFSA',
+  authDomain: 'rnsitregistrationwebapp.firebaseapp.com',
+  databaseURL: 'https://rnsitregistrationwebapp.firebaseio.com',
+  projectId: 'rnsitregistrationwebapp',
+  storageBucket: '',
+  messagingSenderId: '1060794455179'
 }
-function generate()
-{
-  var email = document.getElementById('email').value.toUpperCase();
-  var databaseRef = firebase.database().ref('users/');
-  var db='/users/';
-  databaseRef.once('value', function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
-                var childKey = childSnapshot.key;
-                var childData = childSnapshot.val();  //gets all the child data
-                if(email==childData.email)
-                {
-                  $( ".name" ).html("<p>Name:"+childData.name+"</p>");
-                  $( ".email" ).html("<p>Email:"+childData.email+"</p>");
-                  $( ".phone" ).html("<p>Phone:"+childData.phone+"</p>");
-                  $( ".tid" ).html("<p>Ticket Number:"+childData.tid+"</p>");
-                }
-});
-            });
+firebase.initializeApp(config)
+var id
+var databaseRef = firebase.database().ref('users/')
+var db = '/users/'
+// function save()     //to create ticket number db
+// {
+// var databaseRef = firebase.database().ref('ticketnumber/')
+// var db='/ticketnumber/'
+// var tid=0
+// var data = {
+//             tid:tid
+//         }
+
+//         var updates = {}
+//         updates[db + tid] = data;            // + usn makes it a primary key.
+//         firebase.database().ref().update(updates)
+//         alert('success')
+
+//         reload_page()
+// }
+function tstore () {
+  var databaseRef = firebase.database().ref('ticketnumber/')
+  var db = '/ticketnumber/'
+  var data = {
+    tid: id
+  }
+  var updates = {}
+  updates[db + 0] = data
+  firebase.database().ref().update(updates)
+}
+function save_user () {
+  var databaseRef = firebase.database().ref('ticketnumber/')
+  var db = '/ticketnumber/'
+  databaseRef.once('value', function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      var childData = childSnapshot.val() // gets all the child data
+      id = childData.tid
+    /*alert("first id")
+    alert(id);*/
+    })
+  }).then(function () {
+    var databaseRef = firebase.database().ref('users/')
+    var db = '/users/'
+    var gen = document.getElementById('gender')
+    var mod = document.getElementById('mod')
+    var strgen = gen.options[gen.selectedIndex].text.toUpperCase()
+    var strmod = mod.options[mod.selectedIndex].text.toUpperCase()
+
+    var name = document.getElementById('fname').value.toUpperCase()
+    var email = document.getElementById('email').value.toUpperCase()
+    var phone = document.getElementById('mob').value
+    id = id + 1
+    var tid = 'T' + id
+    var data = {
+      tid: tid,
+      name: name,
+      email: email,
+      phone: phone,
+      gender: strgen,
+      mop: strmod
+    }
+
+    var updates = {}
+    updates[db + tid] = data
+    firebase.database().ref().update(updates)
+    alert('successfully Registered')
+    tstore()
+    change_page()
+  })
+}
+function change_page () {
+  window.location.href = 'ticket.html'
+}
+
+function logout () {
+  firebase.auth().signOut()
+  window.location = 'index.html'
+}
+function generate () {
+  var email = document.getElementById('email').value.toUpperCase()
+  var databaseRef = firebase.database().ref('users/')
+  var db = '/users/'
+  databaseRef.once('value', function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      var childKey = childSnapshot.key
+      var childData = childSnapshot.val() // gets all the child data
+      if (email == childData.email) {
+        $('.name').html('<p>Name:' + childData.name + '</p>')
+        $('.email').html('<p>Email:' + childData.email + '</p>')
+        $('.phone').html('<p>Phone:' + childData.phone + '</p>')
+        $('.tid').html('<p>Ticket Number:' + childData.tid + '</p>')
+      }
+    })
+  })
 }
--- js/index.js
+++ js/index.js
@@ -1,59 +1,56 @@
-$(document).ready(function() {
-    Materialize.updateTextFields();
-  });
- // Initialize Firebase
-    var config = {
-    apiKey: "AIzaSyAqIvSVJoknpvLJHG6Qn0om0JDfGVgJqdE",
-    authDomain: "webapp-faf2d.firebaseapp.com",
-    databaseURL: "https://webapp-faf2d.firebaseio.com",
-    projectId: "webapp-faf2d",
-    storageBucket: "webapp-faf2d.appspot.com",
-    messagingSenderId: "294281955476"
-    };
-    firebase.initializeApp(config);
+$(document).ready(function () {
+  Materialize.updateTextFields()
+})
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyCdptDABvFMyrRx_Sr4Zx7zlbeCtoawFSA',
+  authDomain: 'rnsitregistrationwebapp.firebaseapp.com',
+  databaseURL: 'https://rnsitregistrationwebapp.firebaseio.com',
+  projectId: 'rnsitregistrationwebapp',
+  storageBucket: '',
+  messagingSenderId: '1060794455179'
+}
+firebase.initializeApp(config)
 
 /* log in starts here*/
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // User is signed in.
-    window.location="form.html"
-    alert("Logged in successfully");
-
+    window.location = 'form.html'
+    alert('Logged in successfully')
   } else {
     // No user is signed in.
-    alert("logged out sucessfully");
+    alert('logged out sucessfully')
   }
-});
-function create(){
-      var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-  alert(errorMessage);
-});
-}
-function login(){
-
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
-
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+})
+function create () {
+  var userEmail = document.getElementById('email_field').value
+  var userPass = document.getElementById('password_field').value
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function (error) {
     // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
-
+    var errorCode = error.code
+    var errorMessage = error.message
     // ...
-  });
+    alert(errorMessage)
+  })
+}
+function login () {
+  var userEmail = document.getElementById('email_field').value
+  var userPass = document.getElementById('password_field').value
 
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code
+    var errorMessage = error.message
+
+    window.alert('Error : ' + errorMessage)
+
+  // ...
+  })
 }
 
-function logout(){
-  firebase.auth().signOut();
-  window.location="index.html"
+function logout () {
+  firebase.auth().signOut()
+  window.location = 'index.html'
 }
 /* login ends hrer ................*/
